6bcb08e1305aca426bc538cf97bf0c0dd57682ca
kappa20 <anasdabibe98@gmail.com>
2022-02-06 13:11:10 +0100

    basic dictionary
    no no css only javascript using fetch and
    free public api dictionay

---
+++ dictionaryApi/script.js
@@ -0,0 +1,18 @@
+const apiLink = "https://api.dictionaryapi.dev/api/v2/entries/en/";
+const word = document.getElementById("word");
+const explanation = document.getElementById("explanation")
+
+function search(){
+    let url = apiLink + word.value;
+    fetch(url)
+.then(response => {
+	return response.json();
+}).
+then(json =>{
+    explanation.value = json["0"]["meanings"]["0"]["definitions"]["0"]["definition"] ;
+    console.log("success");
+})
+.catch(err => {
+	console.error(err);
+});
+}
